03804dec617c5492a8f348cf4a24762b6f1a3938
Michael Slomczynski <mike.slomczynski@gmail.com>
2021-10-10 22:58:36 -0500

    Basic Template of the Factory Method Design Pattern.

--- Factory Method/Program.cs
+++ Factory Method/Program.cs
@@ -2,6 +2,41 @@
 
 namespace Factory_Method
 {
+    interface IProduct
+    {
+
+    }
+
+    class ConcreteProductA : IProduct
+    {
+    }
+
+    class ConcreteProductB : IProduct
+    {
+    }
+
+    abstract class Creator
+    {
+        public abstract IProduct FactoryMethod(string type);
+    }
+
+    class ConcreteCreator : Creator
+    {
+        public override IProduct FactoryMethod(string type)
+        {
+            return type switch
+            {
+                "A" => new ConcreteProductA(),
+                "B" => new ConcreteProductB(),
+                _ => throw new ArgumentException("Invalid type", nameof(type)),
+            };
+        }
+    }
+
+
+
+
+
     internal class Program
     {
         static void Main(string[] args)
